bb59e55654333d342ab9c091549131cb1b7cab82
Jhn63 <joaovitorteixeirabarreto150@gmail.com>
2024-05-01 13:45:04 -0300

    menu pronto

--- Cifra_de_transposicao.asm
+++ Cifra_de_transposicao.asm
@@ -11,8 +11,9 @@ includelib \masm32\lib\masm32.lib
 
 .data
     ok_msg db "Processo concluido com sucesso", 0AH, 0H
+    fail_msg db "Ocorreu um erro interno", 0AH, 0H
 
-    str_title db "--------- Crifra de Transposicao ---------", 0AH, 0AH, 0H
+    str_title db "--------- Cifra de Transposicao ---------", 0AH, 0AH, 0H
 
     str_request_3 db "Entre a chave de transposicao", 0AH, 0H  
     str_request_2 db "Entre o caminho do arquivo de saida", 0AH, 0H
@@ -22,16 +23,21 @@ includelib \masm32\lib\masm32.lib
 
     input_buffer db 64 dup(0)
 
+    ;PARAMETROS DA CONSOLE
     input_handle dd 0
     output_handle dd 0
     console_count dd 0
+
+    ;PARAMETROS DO ARQUIVO
+    file_handle dd 0
+    out_file_handle dd 0
     
 .code
 
 ;FUNÇÃO PARA CONVERTER A STRING DE INPUT_BUFFER
 ;PARA UM VALOR NUMERICO, RETORNO EM (EAX)
 
-AsciiToNumeric:
+RemoveCR:
     mov esi, offset input_buffer
     proximo:
         mov al, [esi]
@@ -40,8 +46,7 @@ AsciiToNumeric:
         jne proximo
         dec esi
         xor al, al
-        mov [esi], al
-    invoke atodw, addr input_buffer   
+        mov [esi], al   
 ret
 
 
@@ -52,20 +57,32 @@ Menu:
     invoke WriteConsole, output_handle, addr str_request_1, sizeof str_request_1, addr console_count, NULL
     invoke WriteConsole, output_handle, addr str_prompt, sizeof str_prompt, addr console_count, NULL
     invoke ReadConsole, input_handle, addr input_buffer, sizeof input_buffer, addr console_count, NULL
+    
+    call RemoveCR
+    invoke CreateFile, addr input_buffer, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
+    mov file_handle, eax
 
     invoke WriteConsole, output_handle, addr str_request_2, sizeof str_request_2, addr console_count, NULL
     invoke WriteConsole, output_handle, addr str_prompt, sizeof str_prompt, addr console_count, NULL
     invoke ReadConsole, input_handle, addr input_buffer, sizeof input_buffer, addr console_count, NULL
 
+    call RemoveCR
+    invoke CreateFile, addr input_buffer, GENERIC_READ, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
+    mov out_file_handle, eax
+
     invoke WriteConsole, output_handle, addr str_request_3, sizeof str_request_3, addr console_count, NULL
     invoke WriteConsole, output_handle, addr str_prompt, sizeof str_prompt, addr console_count, NULL
     invoke ReadConsole, input_handle, addr input_buffer, sizeof input_buffer, addr console_count, NULL
+
+    call RemoveCR
+    invoke atodw, addr input_buffer
 
     invoke WriteConsole, output_handle, addr str_options, sizeof str_options, addr console_count, NULL
     invoke WriteConsole, output_handle, addr str_prompt, sizeof str_prompt, addr console_count, NULL
     invoke ReadConsole, input_handle, addr input_buffer, sizeof input_buffer, addr console_count, NULL
 
-    call AsciiToNumeric
+    call RemoveCR
+    invoke atodw, addr input_buffer
 ret
 
 
