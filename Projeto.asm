62f972b49f7700c22886abb78b3b42b8736fa5ed
Jhn63 <joaovitorteixeirabarreto150@gmail.com>
2024-05-04 12:19:39 -0300

    versao final

--- Projeto.asm
+++ Projeto.asm
@@ -8,10 +8,6 @@ include \masm32\include\masm32.inc
 includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib
 
-include \masm32\include\msvcrt.inc
-includelib \masm32\lib\msvcrt.lib
-include \masm32\macros\macros.asm
-
 ;***********************************************************************
 ;| PROJETO DA DISCIPLINA ARQUITETURA DE COMPUTADORES 1 - UFPB - 2024.1 |
 ;|*********************************************************************|
@@ -23,94 +19,98 @@ include \masm32\macros\macros.asm
 
 
 .data
-    opt1_succeed db "Criptografia realizada com sucesso!", 0AH, 0H
-    opt2_succeed db "Descriptografia realizada com sucesso!", 0AH, 0H
-    opt3_succeed db "Fechando programa", 0AH, 0H
+    ;PRINTS DA CONSOLE
+    str_title     db "--------- Cifra de Transposicao ---------", 0AH, 0H                       
+    str_request_3 db 0AH, "Entre a chave de transposicao", 0AH, 0H  
+    str_request_2 db 0AH, "Entre o caminho do arquivo de saida", 0AH, 0H
+    str_request_1 db 0AH, "Entre o caminho do arquivo de entrada", 0AH, 0H
+    str_options   db 0AH, "[1]Criptografar [2]Descriptografar [0]Sair", 0AH, 0H
 
-    str_title db "--------- Cifra de Transposicao ---------", 0AH, 0AH, 0H
+    opt1_succeed db 0AH, "Criptografia realizada com sucesso!", 0AH, 0H
+    opt2_succeed db 0AH, "Descriptografia realizada com sucesso!", 0AH, 0H
+    opt3_succeed db "Fechando programa...", 0AH, 0H
+    str_prompt   db ">> ", 0H
 
-    str_request_3 db "Entre a chave de transposicao", 0AH, 0H  
-    str_request_2 db "Entre o caminho do arquivo de saida", 0AH, 0H
-    str_request_1 db "Entre o caminho do arquivo de entrada", 0AH, 0H
-    str_options   db "[1]Criptografar [2]Descriptografar", 0AH, 0H
-    str_prompt    db ">> ", 0H
 
-    key_numeric dd 8 dup(0)             ;array para receber os digitos da chave
-
-    ;BUFFERS DE LEITURA E ESCRITA
-    console_buffer db 64 dup(0)         ;buffer para ler da console
-    input_file_buffer db 8 dup(0)             ;buffer para ler do arquivo de entrada
-    output_file_buffer db 8 dup(0)             ;buffer para escrever no arquivo de saida
-    
-
-    ;PARAMETROS DA CONSOLE
+    ;PARAMETROS DE ACESSO DA CONSOLE
     input_handle dd 0
     output_handle dd 0
     console_count dd 0
-    
 
-    ;PARAMETROS DO ARQUIVO
+
+    ;PARAMETROS DE ACESSO DOS ARQUIVOS
     input_file_handle dd 0
     output_file_handle dd 0
     count_read dd 0        
     count_write dd 0
+  
+
+    ;BUFFERS DE LEITURA E ESCRITA
+    console_buffer db 50 dup(0)
+    input_file_buffer db 8 dup(0)
+    output_file_buffer db 8 dup(0)
+    
+
+    ;ARRAY PARA RECEBER DIGITOS DA CHAVE
+    key_numeric dd 8 dup(0)
 
 .code
 
-;FUNÇÃO PARA CONVERTER A STRING DE INPUT_BUFFER
-;PARA UM VALOR NUMERICO, RETORNO EM (EAX)
-
-
+;FUNÇÃO PARA CONVERTER STRING DE NUMEROS EM ARRAY DOWRD
 Convert:
     push ebp
     mov ebp, esp
     
-    mov esi, [ebp+8]    ; string
-    mov edi, [ebp+12]   ; array dword
-    xor ecx, ecx        ; Zera o contador ecx
+    mov esi, [ebp+8]                    ;String
+    mov edi, [ebp+12]                   ;Array dword
+    xor ecx, ecx
 
-    loop_start:
-        mov al, [esi + ecx]    ; Carrega o caractere da string key na posição atual em al
-        sub al, '0'            ; Converte o caractere ASCII para valor numérico
-        mov [edi + ecx * 4], al  ; Armazena o número convertido na matriz key_numeric
+    converter:
+        mov al, [esi + ecx]             ;Carregando o caractere da string em al
+        sub al, '0'                     ;Converte o caractere ASCII para valor numerico
+        mov [edi + ecx * 4], al         ;Armazena o valor numerico no array dword
 
-        inc ecx                ; Incrementa o contador
-        cmp ecx, 8             ; Compara o contador com 8
-        jne loop_start         ; Se ecx não for igual a 8, volte ao início do loop
+        inc ecx
+        cmp ecx, 8
+    jne converter
 
     pop ebp
 ret 8
 
 
-;FUNÇÃO PARA REMOVER CR DA STRING
+
+;FUNÇÃO PARA REMOVER CARRIGERETURN DA STRING
 RemoveCR:
     push ebp
     mov ebp, esp
 
-    mov esi, [ebp+8]
+    mov esi, [ebp+8]                    ;String
     proximo:
-        mov al, [esi]
-        inc esi
-        cmp al, 13
+        mov al, [esi]                   ;Move o caracterer atual para al
+        inc esi                         ;Incrementa o indice na String
+        cmp al, 13                      ;Comparando caracterer com valor do CR
         jne proximo
         dec esi
-        xor al, al
-        mov [esi], al
+        xor al, al                      ;Al recebe zero
+        mov [esi], al                   ;Colocando \0 no lugar do CR
         
-        pop ebp
+   pop ebp
 ret 4
 
+
+
+;FUNÇÃO PARA LIMPAR O BUFFER DO ARQUIVO DE ENTRADA
 CleanFileBuffer:
     push ebp
     mov ebp, esp
-    mov esi, [ebp+8]
+    mov esi, [ebp+8]                            ;Buffer do arquivo de entrada
 
-   xor ebx, ebx
-    limpar: 
-        mov byte ptr[esi + 1 * ebx], 0
-        inc ebx
-        cmp ebx, 8
-        jne limpar
+    xor ecx, ecx                                ;Setando contador para 0
+    limpar:
+        mov byte ptr[esi + ecx], 0              ;Colocando 0 no buffer posição ecx (buffer[ecx] = 0)
+        inc ecx                                 ;incrementa contador
+        cmp ecx, 8                              ;loop de 8 iterações
+    jne limpar
 
     pop ebp
 ret 4
@@ -122,86 +122,85 @@ Cipher:
     mov ebp, esp
 
     cifrar:
-        ;LIMPAR BUFFER DE ENTRADA
-        push offset input_file_buffer
-        call CleanFileBuffer
+        push [ebp+8]
+        call CleanFileBuffer                                                                        ;Limpando buffer de entrada
         
-        invoke ReadFile, input_file_handle, addr input_file_buffer, 8, addr count_read, NULL        ;PREENCHE O BUFFER DE ENTRADA COM DADOS DO ARQUIVO DE ENTRADA
-        
-        cmp count_read, 0                                                                           ;SE O CONTADOR ESTIVER EM 0 CHEGAMOS AO FIM DO ARQUIVO
-            je fim_cifrar                                                                           ;PARA CIFRAGEM
+        invoke ReadFile, input_file_handle, [ebp+8], 8, addr count_read, NULL                       ;Preenchendo buffer com dados do arq de entrada
+        cmp count_read, 0                                                                           ;0 caracteres lidos significa fim do arquivo
+            je fim_cifrar                                                                           ;Parar cifragem
 
-        mov esi, offset input_file_buffer                   ;ESI APONTA PARA BUFFER DO ARQ DE ENTADA
-        mov edi, offset output_file_buffer                  ;EDI APONTA PARA BUFFER DO ARQ DE SAIDA
-        xor ecx, ecx                                        ;ZERANDO CONTADOR
+        mov esi, [ebp+8]                                                                            ;Esi aponta para buffer do arq de entrada
+        mov edi, [ebp+12]                                                                           ;Edi aponta para o buffer do arq de saida
+        mov edx, [ebp+16]                                                                           ;Edx aponta para o array dword
+        xor ecx, ecx                                                                                ;Zerando contador
         
-        repetir:                                            ;LAÇO PARA PREENCHER O BUFFER DE SAIDA
-            mov ebx, [key_numeric + ecx * 4]                ;EBX RECEBE UM DIGITO DA CHAVE
-            mov al, [esi + ecx]                             ;AL RECBE UM CARACTER DO BUFFER DE ENTRADA
-            mov byte ptr[edi + 1 * ebx], al                 ;MOVE O CARACTER EM AL PARA O BUFFER DE SAIDA (BUFFER[EBX] = AL)
+        embaralhando:                                                                               ;Laço para preencher buffer de saida
+            mov ebx, [edx + ecx * 4]                                                                ;Ebx recebe um digito da chave
+            mov al, [esi + ecx]                                                                     ;Al recebe um caracter do buffer de entrada
+            mov byte ptr[edi + ebx], al                                                             ;Move o caracter para o buffer de saida (buffer[ebx] = al)
 
             inc ecx
             cmp ecx, 8
-        jne repetir
+        jne embaralhando
 
-        invoke WriteFile, output_file_handle, addr output_file_buffer, 8, addr count_write, NULL    ;DESCARREGA O BUFFER DE SAIDA NO ARQUIVO DE SAIDA
-
+        invoke WriteFile, output_file_handle, [ebp+12], 8, addr count_write, NULL                   ;Descarrega dados do buffer no arq de saida
     jmp cifrar
+    
     fim_cifrar:
+    invoke WriteConsole, output_handle, addr opt1_succeed, sizeof opt1_succeed, addr console_count, NULL
+
     pop ebp
-ret
+ret 12
+
 
     
-;FUNÇÃO PARA DESCIFRAR
-
+;FUNÇÃO PARA DECIFRAR ARQUIVO
 Decipher:
     push ebp
     mov ebp, esp
 
     decifrar:
-        ;LIMPAR BUFFER DE ENTRADA
         push offset input_file_buffer
-        call CleanFileBuffer
+        call CleanFileBuffer                                                                        ;Limpando buffer de entrada
         
-        invoke ReadFile, input_file_handle, addr input_file_buffer, 8, addr count_read, NULL        ;PREENCHE O BUFFER DE ENTRADA COM DADOS DO ARQUIVO DE ENTRADA
-        
-        cmp count_read, 0                                                                           ;SE O CONTADOR ESTIVER EM 0 CHEGAMOS AO FIM DO ARQUIVO
-            je fim_decifrar                                                                         ;PARA CIFRAGEM
+        invoke ReadFile, input_file_handle, [ebp+8], 8, addr count_read, NULL                       ;Preenchendo buffer com dados do arq de entrada
+        cmp count_read, 0                                                                           ;0 caracteres lidos significa fim do arquivo
+            je fim_decifrar                                                                         ;Parar decifragem
 
-        mov esi, offset input_file_buffer                   ;ESI APONTA PARA BUFFER DO ARQ DE ENTADA
-        mov edi, offset output_file_buffer                  ;EDI APONTA PARA BUFFER DO ARQ DE SAIDA
-        xor ecx, ecx                                        ;ZERANDO CONTADOR
+        mov esi, [ebp+8]                                                                            ;Esi aponta para o buffer do arq de entrada
+        mov edi, [ebp+12]                                                                           ;Edi aponta para o buffer do arq de saida
+        mov edx, [ebp+16]                                                                           ;Edx aponta para a array dword
+        xor ecx, ecx                                                                                ;Zerando contador
         
-        repete:                                             ;LAÇO PARA PREENCHER O BUFFER DE SAIDA
-            mov ebx, [key_numeric + ecx * 4]                ;EBX RECEBE UM DIGITO DA CHAVE
-            mov al, [esi + 1 * ebx]                             ;AL RECBE UM CARACTER DO BUFFER DE ENTRADA
-            mov byte ptr[edi + ecx], al                 ;MOVE O CARACTER EM AL PARA O BUFFER DE SAIDA (BUFFER[EBX] = AL)
+        desembaralha:                                                                               ;Laço para preencher o buffer de saida
+            mov ebx, [edx + ecx * 4]                                                                ;Ebx recebe um digito da chave
+            mov al, [esi + ebx]                                                                     ;Al recebe um caracter do buffer de entrada
+            mov byte ptr[edi + ecx], al                                                             ;Move o caracter para o buffer de saida (buffer[ecx] = al)
 
             inc ecx
             cmp ecx, 8
-        jne repete
+        jne desembaralha
 
-        invoke WriteFile, output_file_handle, addr output_file_buffer, 8, addr count_write, NULL    ;DESCARREGA O BUFFER DE SAIDA NO ARQUIVO DE SAIDA
-
+        invoke WriteFile, output_file_handle, [ebp+12], 8, addr count_write, NULL                   ;Descarrega dados do buffer no arq de saida
     jmp decifrar
+    
     fim_decifrar:
+    invoke WriteConsole, output_handle, addr opt2_succeed, sizeof opt2_succeed, addr console_count, NULL
+
     pop ebp
-ret
+ret 12
 
 
 
 ;SOLICITA O ARQUIVO DE ENTRADA E SALVA SEU APONTADOR NA VARIAVEL INPUT_FILE_HANDLE
 RequestInputFile:
-    invoke WriteConsole, output_handle, addr str_title, sizeof str_title, addr console_count, NULL
     invoke WriteConsole, output_handle, addr str_request_1, sizeof str_request_1, addr console_count, NULL
     invoke WriteConsole, output_handle, addr str_prompt, sizeof str_prompt, addr console_count, NULL
     invoke ReadConsole, input_handle, addr console_buffer, sizeof console_buffer, addr console_count, NULL
 
-    ;tratando a string com o nome do arquivo de entrada
     push offset console_buffer
     call RemoveCR
     
-    ;abrindo arquivo
     invoke CreateFile, addr console_buffer, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
     mov input_file_handle, eax
 ret
@@ -214,11 +213,9 @@ RequestOutputFile:
     invoke WriteConsole, output_handle, addr str_prompt, sizeof str_prompt, addr console_count, NULL
     invoke ReadConsole, input_handle, addr console_buffer, sizeof console_buffer, addr console_count, NULL
 
-    ;tratando a string com o nome do arquivo de saida
     push offset console_buffer
     call RemoveCR
 
-    ;criando arquivo
     invoke CreateFile, addr console_buffer, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
     mov output_file_handle, eax
 ret
@@ -231,13 +228,12 @@ RequestKey:
     invoke WriteConsole, output_handle, addr str_prompt, sizeof str_prompt, addr console_count, NULL
     invoke ReadConsole, input_handle, addr console_buffer, sizeof console_buffer, addr console_count, NULL
 
-    ;tratando string com a chave de transposição 
     push offset console_buffer
     call RemoveCR
 
-    push offset key_numeric
-    push offset console_buffer
-    call Convert                ;converte a chave de string de dword
+    push offset key_numeric                         ;Array dword de 8 posições
+    push offset console_buffer                      ;String de numeros sem carrige return
+    call Convert                                    ;Convertendo string de numeros em array dword
 ret
 
 
@@ -248,43 +244,48 @@ ShowOptions:
     invoke WriteConsole, output_handle, addr str_prompt, sizeof str_prompt, addr console_count, NULL
     invoke ReadConsole, input_handle, addr console_buffer, sizeof console_buffer, addr console_count, NULL
 
-    ;tratando string com opção escolhida
     push offset console_buffer
     call RemoveCR
 
-    ;convertendo o valor em numerico, retorno em eax
+    ;Convertendo ASCII em Dword, retorno em eax
     invoke atodw, addr console_buffer
 ret
 
 ;MOSTRA O MENU DO PROGRAMA
 Menu:
-    call RequestInputFile
-    call RequestOutputFile
-    call RequestKey
-    call ShowOptions
+    call ShowOptions                            ;Mostrando opções
         
-    cmp eax, 1
-        je case_1
-    cmp eax, 2
-        je case_2
+    cmp eax, 1                                  ;Estrutura de instruções que simula um switch case
+        je caso_1                               ;Opção 1 Cifragem
+    cmp eax, 2                                  ;Opção 2 Decifragem
+        je caso_2
+        
+    jmp fim_casos                               ;Qualquer outra entrada fecha o programa
+        caso_1:
 
-    jmp end_case
-        case_1:
-  
-            call Cipher   
+            call RequestInputFile
+            call RequestOutputFile
+            call RequestKey
 
-            jmp end_case
+            push offset key_numeric
+            push offset output_file_buffer
+            push offset input_file_buffer
+            call Cipher
 
-       case_2:
-       
+            jmp fim_casos
+       caso_2:
+
+            call RequestInputFile
+            call RequestOutputFile
+            call RequestKey
+
+            push offset key_numeric
+            push offset output_file_buffer
+            push offset input_file_buffer
             call Decipher
-            jmp end_case
-
-       end_case:
+            
+     fim_casos:
 ret
-
-
-;O PROGRAMA FECHA QUANDO "SAIR" FOR SOLICITADO
 
 start:
     invoke GetStdHandle, STD_INPUT_HANDLE
@@ -292,6 +293,7 @@ start:
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov output_handle, eax
 
+    invoke WriteConsole, output_handle, addr str_title, sizeof str_title, addr console_count, NULL
     call Menu
 
     invoke CloseHandle, input_file_handle
